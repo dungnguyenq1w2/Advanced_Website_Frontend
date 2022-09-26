b1668ece318ff138f75bb5c3d3427e25513c2b2c
Nguyễn Tấn Dũng <dungnguyenq1w2@gmail.com>
2022-09-26 09:26:23 +0700

    update: set color for X and O

--- src/modules/tic-tac-toe/components/Square/index.js
+++ src/modules/tic-tac-toe/components/Square/index.js
@@ -5,7 +5,10 @@ import React from 'react'
 function Square({ value, isWin, onClick }) {
 	// console.log(isWin)
 	return (
-		<button className={`square ${isWin && 'win'}`} onClick={onClick}>
+		<button
+			className={`square ${isWin && 'win'} ${value === 'X' ? 'x-style' : 'o-style'}`}
+			onClick={onClick}
+		>
 			{value}
 		</button>
 	)
